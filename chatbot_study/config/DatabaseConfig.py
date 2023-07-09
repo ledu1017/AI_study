0501058c67a9def7cf8a886ed972bfbc1afa842d
ledu1017 <ledu202@naver.com>
2023-07-09 23:58:08 +0900

    07.09

---
+++ chatbot_study/config/DatabaseConfig.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+DB_HOST = "127.0.0.1"
+DB_USER = "root"
+DB_PASSWORD = "doudu10170%"
+DB_NAME = "chat_bot"
+
+def DatabaseConfig():
+    global DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+
